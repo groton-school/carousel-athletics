45d17d19e03f3338015811cfe2fa3a8059c9ddf6
Seth Battis <sbattis@groton.org>
2024-02-03 09:43:48 -0500

    RSS feed
    Fewer available fields, but less date munging _and_ less weird grouping artifacts in Carousel

---
+++ src/server/Workflows/rss.php
@@ -0,0 +1,101 @@
+<?php
+
+use FeedWriter\RSS2;
+use GrotonSchool\AthleticsSchedule\Blackbaud\Schedule;
+use GrotonSchool\AthleticsSchedule\Blackbaud\SKY;
+use GrotonSchool\AthleticsSchedule\Blackbaud\Team;
+use Ramsey\Uuid\Uuid;
+
+require __DIR__ . '/../../../vendor/autoload.php';
+
+session_start();
+date_default_timezone_set('America/New_York');
+
+if (!SKY::isReady($_SERVER, $_SESSION, $_GET)) {
+    echo json_encode(['error' => 'not authenticated']);
+    exit();
+}
+
+$params = $_GET;
+function extractParam($key, $default = null)
+{
+    global $params;
+    if (!empty($params[$key])) {
+        $value = $params[$key];
+        unset($params[$key]);
+        return $value;
+    } else {
+        return $default;
+    }
+}
+
+$title = extractParam('title');
+$title_position = extractParam('title_position');
+$hide_scoreless = extractParam(
+    'hide_scoreless',
+    false,
+    fn ($val) => $val == 'true'
+);
+$tzid = extractParam('tzid', 'America/New_York');
+$per_page = extractParam('per_page');
+$team_id = empty($params['team_id']) ? null : $params['team_id'];
+
+$schedule = Schedule::get($params);
+
+$start = $schedule->getFirst() ? $schedule->getFirst()->getDate() : '';
+$end = $schedule->getLast() ? $schedule->getLast()->getDate() : '';
+$baseTitle = $team_id
+    ? Team::get($team_id)->getName()
+    : $start . ($start != $end ? ' – ' . $end : '');
+switch ($title_position) {
+    case 'prepend':
+        $title = "$title$baseTitle";
+        break;
+    case 'replace':
+        break;
+    case 'append':
+        $title = "$baseTitle$title";
+        break;
+    default:
+        $title = $baseTitle;
+}
+
+$feed = new RSS2();
+$feed->setTitle($title);
+$feed->setDescription(json_encode($params));
+$feed->setLink(
+    'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'],
+    'self'
+);
+$feed->setAtomLink(
+    'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'],
+    'self'
+);
+
+foreach ($schedule->items as $item) {
+    if (!$hide_scoreless || ($hide_scoreless && !empty($item->getScore()))) {
+        $event = $feed->createNewItem();
+        $event->setDate($item->getDate('r'));
+        $event->addElement('pubDate', $item->getDate('r'));
+        $event->addElement('category', $item->getTeamName());
+        $event->setDescription($item->getOpponentName());
+        $event->setTitle(
+            $item->isFuture()
+                ? $item->getStart() .
+                    (empty($item->getHomeOrAway())
+                        ? ''
+                        : ' (' . $item->getHomeOrAway() . ')')
+                : $item->getScore() .
+                    (empty($item->getOutcome())
+                        ? ''
+                        : ' (' . $item->getOutcome() . ')')
+        );
+        $event->setId(Uuid::uuid4());
+
+        $feed->addItem($event);
+    }
+}
+
+header('Content-Type: application/rss+xml');
+echo $feed->generateFeed();
+exit();
